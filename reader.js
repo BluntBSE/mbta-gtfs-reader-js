346d14655a85a439536ef9654f9aa805a207e246
Robert Meyer <robert@robertmeyerwrites.com>
2021-12-25 20:15:36 -0500

    Updated readme

--- reader.js
+++ reader.js
@@ -1,6 +1,6 @@
 // There are perfectly good libraries for reading GTFS files, but reading data out of files and moving it around correctly seems like a pretty big deal at the MBTA, so
 // I've ignored them and decided to party instead
-//When called, this script creates an object out of a GTFS .txt file with keys that correspond to the first column of the data (almost always an id)
+//When called, this script creates an object out of a GTFS .txt file with keys that correspond to the first column of the data (almost always an id). 'Trips' is the exception.
 //Example:
 //const stopsData = parseGTFS("./extracted/", "stops.txt",);
 //console.log(stopsData['node-wtcst-wtctstairs-top']);
